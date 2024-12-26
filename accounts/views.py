4ccb7a0372f615a2271a5e870c783c9f9acf3b38
ysooj <musukie52@gmail.com>
2024-12-26 20:29:04 +0900

    feat: 로그인 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
 from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
 
 # 회원가입
 def signup_view(request):
@@ -14,3 +15,20 @@ def signup_view(request):
         form = SignupForm()
 
     return render(request, 'accounts/signup.html', {'form':form})
+
+# 로그아웃
+def login_view(request):
+    # 유저가 인증되었다면 바로 상품 목록 보여줘라.
+    if request.user.is_authenticated:
+        return redirect('products:product_list')
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('products:product_list')
+        
+    else:
+        form = AuthenticationForm(request)
+        return render(request, 'accounts/login.html', {'form': form})
