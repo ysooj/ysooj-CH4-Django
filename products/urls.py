6c419c1b9ea1eeda4897d940d1e3cc8cb7950d80
ysooj <musukie52@gmail.com>
2024-12-26 20:38:54 +0900

    feat: 상품 상세 수정 기능 구현

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.product_list_view, name='product_list'),
     path('create/', views.product_create_view, name='product_create'),
     path('<int:pk>/', views.product_detail_view, name='product_detail'),
+    path('<int:pk>/update/', views.product_update_view, name='product_update'),
 ]
--- products/views.py
+++ products/views.py
@@ -28,3 +28,18 @@ def product_detail_view(request, pk):
     product.views += 1
     product.save()
     return render(request, 'products/product_detail.html', {'product': product})
+
+@login_required
+def product_update_view(request, pk):
+    product = get_object_or_404(Product, pk=pk) # 항상 검증 필수! 해당 상품이 db에 있는지!
+    if product.user != request.user:    # 해당 상품을 등록한 유저가, 지금 로그인한 유저인지 검증!
+        return redirect('products:product_detail', pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('products:product_detail', pk=pk)
+    else:   # post 요청이 아닌 get 요청일 때.
+        form = ProductForm(instance=product, initial={'hashtags_str': ' '.join(ht.name for ht in product.hashtags.all())})
+
+    return render(request, 'products/product_form.html', {'form': form, 'product':product})
